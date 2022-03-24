dc3fdb02e3791c3f69ea124bf88c123c8611a192
Auliaaa2108 <90377392+Auliaaa2108@users.noreply.github.com>
2022-03-24 16:24:35 +0700

    qwert

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require("path");
-const router = require("./router/router");
+const router = require("./Router/mahasiswa");
 
 app.use(router);
 
