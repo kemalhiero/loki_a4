3b2cc6a9191f9851380fdf6de51456cc3ba8f562
Auliaaa2108 <90377392+Auliaaa2108@users.noreply.github.com>
2022-03-24 20:02:04 +0700

    masih usaha

--- Router/mahasiswa.js
+++ Router/mahasiswa.js
@@ -1,35 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
-//request halaman depan
-router.get("/", (req, res) => {
-  res.send("Selamat Datang (> <)");
+//route mengubah/membuat RPS yang sudah ada didalam Web
+router.post("inirps", (req, res) => {
+  console.log("UBAH DATA RPS");
+  let obj = 
+  {
+      "message": "Data diubah oleh Dosen",
+      "Nama": "Doraemon S. Kom",
+      "Kode Matakuliah": "SI1892",
+      "Mata kuliah ": "Dasar Program",
+      "RPS": "(Isi RPS)",
+      "Ket": "BERHASIL",
+      "code": 200
+  }
+  res.json(obj);
+  console.log("\n\nRPS berhasil di Update");
 });
 
-//request halaman login
-router.get("/login", (req, res) => {
-  res.send("<h3>Ini adalah Halaman login</h3>");
-});
-
-//request halaman homepage (beranda)
-router.get("/home", (req, res) => {
-  res.send("Ini adalah Halaman Home");
-});
-
-//request halaman about
-router.get("/about", (req, res) => {
-  res.send("Ini Adalah Halaman about");
-});
-
-//request halaman profil
-router.get("/profile", (req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Halooo");
-});
-
-//request halaman salam, nama, nim
-router.get("/salam/:nama/:nim", function (req, res) {
-  res.send("Selamat Sore " + req.params.nama + " <br>nim anda " + req.params.nim);
-});
 
 module.exports = router;
