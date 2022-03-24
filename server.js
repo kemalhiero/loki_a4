49663398a1d1e3b1a717169b60a10cfa5515750b
Radithya Romero Al Giffary <radithya.romero@gmail.com>
2022-03-24 16:48:00 +0700

    testt

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require("path");
-const router = require("./router/router");
+const router = require("./router/mahasiswa");
 
 app.use(router);
 
-//
 app.listen(port, () =>{
     console.log(`example app listening on port ${port}`)
 });
