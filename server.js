69f15cac24ec917a1dd440dada1628fe262da60e
Auliaaa2108 <90377392+Auliaaa2108@users.noreply.github.com>
2022-03-24 20:01:09 +0700

    coba

--- server.js
+++ server.js
@@ -2,12 +2,38 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require("path");
-const router = require("./router/router");
+// const router = require("./Router/mahasiswa");
 
-app.use(router);
-
-//
-app.listen(port, () =>{
-    console.log(`example app listening on port ${port}`)
+app.get("/", (req, res) => 
+{
+    res.send("Welcome di Server JS");
+});
+  
+app.get("/login", (req, res) => 
+{
+    res.send("Ini adalah Halaman Login");
 });
 
+app.get("/logout", (req, res) => 
+{
+    res.send("Ini merupakan halaman logout :)");
+});
+  
+app.get("/daftarrps", (req, res) => 
+{
+    res.send("Daftar RPS");
+});
+
+app.get("/print", (req, res) => 
+{
+    res.send("Cetak RPS");
+});
+
+app.get("/lihatrps", (req, res) => 
+{
+    res.send("RPS dapat dilihat di sini");
+});
+
+app.listen(port, () =>{
+ console.log(`hdhjsfhjPort lagi jalan`)
+});
