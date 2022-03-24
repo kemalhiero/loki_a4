bc559c7a3e7153de42fa742634ae1d429cb130fa
2011521013_PutiJugalo <2011521013_puti@student.unand.ac.id>
2022-03-24 17:22:55 +0700

    Tessttt

--- server.js
+++ server.js
@@ -7,19 +7,29 @@ const router = require("./Router/mahasiswa");
 app.get("/", (req, res) => 
 {
     res.send("Welcome di Server JS");
-  });
+});
   
 app.get("/login", (req, res) => 
 {
-
     res.send("Ini adalah Halaman Login");
 });
 
-app.get("/logout", (req, res) => {
+app.get("/logout", (req, res) => 
+{
     res.send("Ini merupakan halaman logout :)");
-  });
+});
   
 app.get("/daftarrps", (req, res) => 
 {
     res.send("Daftar RPS");
+});
+
+app.get("/print", (req, res) => 
+{
+    res.send("Cetak RPS");
+});
+
+app.get("/lihatrps", (req, res) => 
+{
+    res.send("RPS dapat dilihat di sini");
 });
