a871b68385019c673283d43aeb9f02bcfe843161
hagisiraj <79428267+hagisiraj@users.noreply.github.com>
2022-04-18 14:46:45 +0700

    Modul jwt dan model

---
+++ models/course_plans_details.js
@@ -0,0 +1,64 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require("./dbconfig");
+  
+const course_plans_details = sequelize.define('course_plans_details' ,
+{
+    id:
+    {
+        type : DataTypes.BIGINT,
+        allowNull: false,
+        primaryKey : true,
+        autoIncrement: true
+    },
+
+    course_plan_id:
+    {
+        type : DataTypes.BIGINT,
+        allowNull: false
+    },
+
+    week:
+    {
+        type : DataTypes.INTEGER,
+        allowNull: false 
+    }, 
+
+    material:
+    {
+        type : DataTypes.TEXT,
+        allowNull: false 
+    },
+
+    method:
+    {
+        type : DataTypes.TEXT,
+        allowNull: false
+    },
+
+    student_experience:
+    {
+        type : DataTypes.TEXT,
+        allowNull: false
+    },
+
+    created_at:
+    {
+        type : DataTypes.DATE,
+    },
+
+    updated_at:
+    {
+        type : DataTypes.DATE,
+
+    }
+    }, 
+    
+    {
+    tableName: 'course_plans_details',
+    timestamps: true,
+    updatedAt: 'updated_at',
+    createdAt: 'created_at'
+
+});
+
+module.exports = course_plans_details;
