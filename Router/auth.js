aeb3fc8890b7ea9b70aba4b29157b19d0c51d8bc
2011521013_PutiJugalo <2011521013_puti@student.unand.ac.id>
2022-05-17 17:30:08 +0700

    set view engine

--- Router/auth.js
+++ Router/auth.js
@@ -6,4 +6,9 @@ router.post("/register", controller.users.register);
 router.post("/login", controller.users.login);
 router.delete("/logout", controller.users.logout);
 
+router.get("/login", (req, res) => 
+{
+    res.render('auth-login');
+});
+
 module.exports = router;
