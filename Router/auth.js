97b19d3fed77f16c547e707cf2c1910a727ca560
Radithya Romero Al Giffary <radithya.romero@gmail.com>
2022-05-31 15:49:12 +0700

    register

--- Router/auth.js
+++ Router/auth.js
@@ -1,17 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express();
 const controller = require(`../controllers/indexcontroller`);
 
-router.set('view engine', 'ejs');
-router.use( express.static( "views" ) );
+router.set("view engine", "ejs");
+router.use(express.static("views"));
 
 router.post("/register", controller.users.register);
 router.post("/login", controller.users.login);
 router.delete("/logout", controller.users.logout);
 
-router.get("/login", (req, res) => 
-{
-    res.render('auth-login');
+router.get("/login", (req, res) => {
+  res.render("auth-login");
 });
 
+router.get("/register", (req, res) => {
+  res.render("register");
+});
 module.exports = router;
