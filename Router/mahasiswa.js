f965cdd92f571e513cd784d8d177ecb7d850aded
Radithya Romero Al Giffary <radithya.romero@gmail.com>
2022-06-03 11:40:45 +0700

    😘😘😘

--- Router/mahasiswa.js
+++ Router/mahasiswa.js
@@ -3,20 +3,24 @@ const router = express();
 const controller = require(`../controllers/indexcontroller`);
 const authenticateToken = require(`../middleware/authToken`);
 
-router.set('view engine', 'ejs');
-router.use( express.static( "views" ) );
+router.set("view engine", "ejs");
+router.use(express.static("views"));
 
 //route mengubah/membuat RPS yang sudah ada didalam Web
-router.get("/lihatdetailrps", (req, res) => {
-  const role = req.cookies.role
-  const nama = req.cookies.nama
-  res.render('rpsmahasiswa',{role:role, nama:nama, dasbordaktif:"", rpsaktif:"active"});
+router.get("/rps", (req, res) => {
+  const role = req.cookies.role;
+  const nama = req.cookies.nama;
+  res.render("rpsmahasiswa", { role: role, nama: nama, dasbordaktif: "", rpsaktif: "active" });
 });
 
+router.get("/caridirps", (req, res) => {
+  res.send("Ni halaman cari RPS");
+});
 
-router.get("/caridirps", (req, res) => 
-{
-    res.send("Ni halaman cari RPS");
+router.get("/detailrps", (req, res) => {
+  const role = req.cookies.role;
+  const nama = req.cookies.nama;
+  res.render("detailrpsmhs", { role: role, nama: nama, dasbordaktif: "", rpsaktif: "active" });
 });
 
 module.exports = router;
